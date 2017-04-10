baeba6b6028c4fe6abb308c5095a45ef1c821dde
MangalBeta <mangalhcl449@gmail.com>
2017-04-11 01:24:27 +0530

    first commit

---
+++ server/_config.js
@@ -0,0 +1,19 @@
+var ids = {
+  github: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  linkedin: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+  },
+  twitter: {
+    consumerKey: 'yuGqgF50LeyoLBXr5tGoB1U0F',
+    consumerSecret: 'uRQx59upcqiz5NL2HtVpnY34wK2kZBtojb9VDe2AmSOPvMXQ86',
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+  }
+};
+
+module.exports = ids;
